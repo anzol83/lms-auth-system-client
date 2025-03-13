9a55aababf0e0e2730b0ab51a8adb6edda5799e4
Angel Neupane <angelneupane83@gmail.com>
2025-03-13 22:35:39 +1100

    hooks

---
+++ src/pages/Auth/VerifyUserPage.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import { toast } from "react-toastify";
+import { verifyUser } from "../../axios/userAxios";
+
+const VerifyUserPage = () => {
+  // Extract the info from URL
+  const [params] = useSearchParams()
+
+  const userEmail = params.get('email')
+  const token = params.get('token')
+console.log("userEmail", userEmail, token);
+
+  // Send Request to API for verifying the user
+  const verifyUserEmail = async() => {
+    const result = await verifyUser(userEmail, token)
+
+    // Handle error
+    if(result.status === "error"){
+      toast.error(result.message)
+    }
+
+    // Success
+    toast.success(result.message)
+  }
+
+  useEffect(() => {
+    if(userEmail && token){
+      verifyUserEmail()
+    }
+  }, [])
+
+  return ( 
+    <h1>Hello, I will verify the user</h1>
+   );
+}
+ 
+export default VerifyUserPage;
